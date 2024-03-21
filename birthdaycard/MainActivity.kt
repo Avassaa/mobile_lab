11e99dbb3102db4c38512a6ed630103fc0a427ff
Avassaa <119755728+Avassaa@users.noreply.github.com>
2024-03-22 00:58:29 +0300

    birthday card

---
+++ birthdaycard/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.example.birthdaycard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycard.ui.theme.BirthdayCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingText(
+                        message = "Happy Birthday Baha!",
+                        from = "from İrem",
+                        modifier=Modifier.padding(8.dp))
+                }
+            }
+        }
+    }
+}
+@Composable
+fun GreetingText(message:String,from:String,modifier:Modifier=Modifier){
+    Column(
+        verticalArrangement= Arrangement.Center,
+        modifier=modifier
+            .background(Color.Red)
+
+
+
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier=modifier
+                .padding(16.dp)
+                .align(Alignment.End)
+            //verticalArrangement ise kendi childlarının kendi içindeki dikey konumunu ayarlıyor
+            //textAlign sadece texti taşır , fillmaxwidth ile kombolayınca kutuyu büyütüp sağa taşıyor
+            //text align texti kendi layoutu içinde taşıyor
+        //modifier.align ise kutuyu direkt taşıyor (bütün composable'yi parenti içinde)
+            //modifieri parametre olarak verince
+        )
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun BirthdayCardPreview() {
+    BirthdayCardTheme {
+        GreetingText(message = "Happy Birthday Baha!", from = "from İrem")
+    }
+}
