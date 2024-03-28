18edd354fa47fe769539805cbea604df46cf30ed
Avassaa <119755728+Avassaa@users.noreply.github.com>
2024-03-28 20:51:34 +0300

    Add files via upload
    update on mainActivity.kt

--- birthdaycard/MainActivity.kt
+++ birthdaycard/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.birthdaycard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,12 +20,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.birthdaycard.ui.theme.BirthdayCardTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,43 +41,67 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingText(
-                        message = "Happy Birthday Baha!",
-                        from = "from İrem",
-                        modifier=Modifier.padding(8.dp))
+                    GreetingImage(
+                        message = stringResource(R.string.happy_birthday_irem),
+                        from = stringResource(R.string.from_baha),
+                        modifier=Modifier)
                 }
             }
         }
     }
 }
 @Composable
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box(modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha=0.65f
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+}
+
+
+
+@Composable
 fun GreetingText(message:String,from:String,modifier:Modifier=Modifier){
+
     Column(
         verticalArrangement= Arrangement.Center,
         modifier=modifier
-            .background(Color.Red)
-
-
-
     ) {
         Text(
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
-            textAlign = TextAlign.Center
+            textAlign = TextAlign.Center,
         )
         Text(
             text = from,
             fontSize = 36.sp,
-            modifier=modifier
+            modifier= Modifier
                 .padding(16.dp)
-                .align(Alignment.End)
+                .align(Alignment.CenterHorizontally)
             //verticalArrangement ise kendi childlarının kendi içindeki dikey konumunu ayarlıyor
             //textAlign sadece texti taşır , fillmaxwidth ile kombolayınca kutuyu büyütüp sağa taşıyor
             //text align texti kendi layoutu içinde taşıyor
         //modifier.align ise kutuyu direkt taşıyor (bütün composable'yi parenti içinde)
             //modifieri parametre olarak verince
+            //align row ise verticalAlignment
+            //column ise horizontalAlignmenti overridelar
         )
+
+
+
     }
 }
 
@@ -80,6 +111,6 @@ fun GreetingText(message:String,from:String,modifier:Modifier=Modifier){
 @Composable
 fun BirthdayCardPreview() {
     BirthdayCardTheme {
-        GreetingText(message = "Happy Birthday Baha!", from = "from İrem")
+        GreetingImage(message = stringResource(R.string.happy_birthday_irem), from = stringResource(R.string.from_bahaa) )
     }
 }
